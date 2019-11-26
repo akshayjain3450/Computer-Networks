70c09e2fe286bb77cffb9486e7900767c86e5098
AKSHAY <akshayjain3450@gmail.com>
2019-11-26 21:06:18 +0530

    CSMA TCP using NS3

---
+++ scratch folder/csma_tcp.cc
@@ -0,0 +1,155 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+// Network topology
+//
+//       n0    n1   n2   n3
+//       |     |    |    |
+//       =================
+//              LAN
+//
+// - UDP flows from n0 to n1 and back
+// - DropTail queues
+// - Tracing of queues and packet receptions to file "udp-echo.tr"
+
+#include <fstream>
+#include "ns3/core-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/flow-monitor-module.h"
+
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("UdpEchoExample");
+
+
+void printStats (FlowMonitorHelper &flowmon_helper, bool perFlowInfo) {
+  Ptr<Ipv4FlowClassifier> classifier =
+DynamicCast<Ipv4FlowClassifier>(flowmon_helper.GetClassifier());
+  std::string proto;
+  Ptr<FlowMonitor> monitor = flowmon_helper.GetMonitor ();
+  std::map < FlowId, FlowMonitor::FlowStats > stats = monitor->GetFlowStats();
+  double totalTimeReceiving;
+  uint64_t totalPacketsReceived, totalPacketsDropped,
+totalBytesReceived,totalPacketsTransmitted;
+
+  totalBytesReceived = 0, totalPacketsDropped = 0,
+totalPacketsReceived = 0, totalTimeReceiving =
+0,totalPacketsTransmitted = 0;
+  for (std::map< FlowId, FlowMonitor::FlowStats>::iterator flow =
+stats.begin(); flow != stats.end(); flow++)
+  {
+    Ipv4FlowClassifier::FiveTuple  t = classifier->FindFlow(flow->first);
+    switch(t.protocol)
+     {
+     case(6):
+         proto = "TCP";
+         break;
+     case(17):
+         proto = "UDP";
+         break;
+     default:
+         exit(1);
+     }
+     totalBytesReceived += (double) flow->second.rxBytes * 8;
+     totalTimeReceiving += flow->second.timeLastRxPacket.GetSeconds ();
+     totalPacketsReceived += flow->second.rxPackets;
+     totalPacketsDropped += flow->second.txPackets - flow->second.rxPackets;
+     totalPacketsTransmitted += flow->second.txPackets;
+     if (perFlowInfo)
+     {
+      std::cout << "FlowID: " << flow->first << " (" << proto << " "
+                 << t.sourceAddress << " / " << t.sourcePort << " --> "
+                 << t.destinationAddress << " / " << t.destinationPort << ")" << std::endl;
+       std::cout << "  Tx Bytes: " << flow->second.txBytes << std::endl;
+std::cout << "  Lost Packets: " << flow->second.lostPackets << std::endl;
+       std::cout << "  Pkt Lost Ratio: " <<((double)flow->second.txPackets-(double)flow->second.rxPackets)/(double)flow->second.txPackets << std::endl;
+       std::cout << "  Throughput: " << (((double)flow->second.rxBytes * 8) /(flow->second.timeLastRxPacket.GetSeconds ()) ) << "bits/s" <<std::endl;
+       std::cout << "  Mean{Delay}: " <<(flow->second.delaySum.GetSeconds()/flow->second.rxPackets) <<std::endl;
+       std::cout << "  Mean{Jitter}: " <<(flow->second.jitterSum.GetSeconds()/(flow->second.rxPackets)) <<std::endl;
+     }
+}
+
+     std::cout<< "Final throughput with (packets received/total time): "<<(totalPacketsReceived * 1024 * 8)/totalTimeReceiving<<" bps "<<std::endl;
+     std::cout<< "Final throughput with (packets received/total time): "<<(totalBytesReceived)/totalTimeReceiving<<" bps "<<std::endl;
+     std::cout<<"Total packets transmitted:"<<totalPacketsTransmitted<<std::endl;
+     std::cout<<"Total packets received: "<< totalPacketsReceived<<std::endl;
+     std::cout<<"Total packets dropped: "<< totalPacketsDropped<<std::endl;
+     std::cout << "Packet Lost Ratio: " << totalPacketsDropped /(double) (totalPacketsReceived + totalPacketsDropped) << std::endl;
+}
+
+
+
+int
+main (int argc, char *argv[])
+{
+
+  Time::SetResolution (Time::NS);
+  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+  NS_LOG_INFO ("Create nodes.");
+  NodeContainer csmaNodes;
+  csmaNodes.Create (20);
+
+  InternetStackHelper internet;
+  internet.Install (csmaNodes);
+
+  CsmaHelper csma;
+  csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (5000000)));
+  csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
+  csma.SetDeviceAttribute ("Mtu", UintegerValue (1400));
+
+
+  NetDeviceContainer devices ;
+  devices = csma.Install (csmaNodes);
+
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+  int port = 9;
+for(int i = 0;i<20;i++)
+{  BulkSendHelper source ("ns3::TcpSocketFactory",InetSocketAddress (interfaces.GetAddress (1), port));
+  // Set the amount of data to send in bytes.  Zero is unlimited.
+  source.SetAttribute ("MaxBytes", UintegerValue (0));
+
+  ApplicationContainer sourceApps = source.Install (csmaNodes.Get (i));
+  sourceApps.Start (Seconds (0.0));
+  sourceApps.Stop (Seconds (10.0));}
+
+//
+// Create a PacketSinkApplication and install it on node 1
+//
+  PacketSinkHelper sink ("ns3::TcpSocketFactory",InetSocketAddress (Ipv4Address::GetAny (), port));
+  ApplicationContainer sinkApps = sink.Install (csmaNodes.Get (1));
+  sinkApps.Start (Seconds (0.0));
+  sinkApps.Stop (Seconds (10.0));
+
+
+  Ptr<FlowMonitor> flowmon;
+    FlowMonitorHelper flowmonHelper;
+    flowmon = flowmonHelper.InstallAll ();
+
+  Simulator::Stop (Seconds(10.1));
+  Simulator::Run ();
+
+  printStats (flowmonHelper, true);
+  Simulator::Destroy ();
+  NS_LOG_INFO ("Done.");
+}
